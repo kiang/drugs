9c8d8145eb086a5bd5c5be7fdc038e42ad4dc52b
Finjon Kiang <kiange@gmail.com>
2015-02-28 00:27:16 +0800

    search form for points

--- webroot/js/c/points/index.js
+++ webroot/js/c/points/index.js
@@ -1,8 +1,14 @@
 $(function () {
     $('a.btn-find-point').click(function () {
+        $('form#form-find-point').trigger('submit');
+        return false;
+    });
+    $('form#form-find-point').submit(function () {
         var keyword = $('input#pointKeyword').val();
         if (keyword !== '') {
             location.href = baseUrl + 'points/index/' + encodeURIComponent(keyword);
+        } else {
+            alert('您尚未輸入關鍵字！');
         }
         return false;
     });
