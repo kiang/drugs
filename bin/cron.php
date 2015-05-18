b138914eff8d951236136c3b8866ba7677869ae6
Finjon Kiang <kiange@gmail.com>
2015-05-19 01:49:54 +0800

    replace exec() using system()

--- bin/cron.php
+++ bin/cron.php
@@ -3,18 +3,18 @@
 $path = dirname(__DIR__);
 $now = date('Y-m-d H:i:s');
 
-exec("cd {$path} && /usr/bin/git pull");
+system("cd {$path} && /usr/bin/git pull");
 
-exec("/usr/bin/php -q {$path}/cake2/lib/Cake/Console/cake.php import update -working {$path}");
+system("/usr/bin/php -q {$path}/cake2/lib/Cake/Console/cake.php import update -working {$path}");
 
-exec("/usr/bin/php -q {$path}/cake2/lib/Cake/Console/cake.php mohw update -working {$path}");
+system("/usr/bin/php -q {$path}/cake2/lib/Cake/Console/cake.php mohw update -working {$path}");
 
-exec("/usr/bin/php -q {$path}/cake2/lib/Cake/Console/cake.php mohw import -working {$path}");
+system("/usr/bin/php -q {$path}/cake2/lib/Cake/Console/cake.php mohw import -working {$path}");
 
-exec("/usr/bin/php -q {$path}/cake2/lib/Cake/Console/cake.php import dump -working {$path}");
+system("/usr/bin/php -q {$path}/cake2/lib/Cake/Console/cake.php import dump -working {$path}");
 
-exec("cd {$path} && /usr/bin/git add -A");
+system("cd {$path} && /usr/bin/git add -A");
 
-exec("cd {$path} && /usr/bin/git commit --author 'auto commit <noreply@localhost>' -m 'auto update @ {$now}'");
+system("cd {$path} && /usr/bin/git commit --author 'auto commit <noreply@localhost>' -m 'auto update @ {$now}'");
 
-exec("cd {$path} && /usr/bin/git push origin master");
+system("cd {$path} && /usr/bin/git push origin master");
